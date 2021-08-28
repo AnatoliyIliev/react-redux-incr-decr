518fb042a19e808158cbfbf981afcb690f9c7244
Anatoliy Iliev <vekok12@gmail.com>
2021-08-28 19:36:20 +0300

    day-1

--- src/App.js
+++ src/App.js
@@ -1,29 +1,5 @@
-import React from 'react';
-import { Switch, Route, Link } from 'react-router-dom';
-import TodosView from './views/TodosView';
-import CounterView from './views/CounterView';
+import Counter from './components/Counter/Counter';
 
-const App = () => (
-  <>
-    <ul>
-      <li>
-        <Link to="/counter">Счётчик</Link>
-      </li>
-      <li>
-        <Link to="/todos">Заметки</Link>
-      </li>
-    </ul>
-
-    <Switch>
-      <Route path="/counter">
-        <CounterView />
-      </Route>
-
-      <Route path="/todos">
-        <TodosView />
-      </Route>
-    </Switch>
-  </>
-);
+const App = () => <Counter />;
 
 export default App;
--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { connect } from 'react-redux';
+// import { connect } from 'react-redux';
 import Controls from './Controls';
 import Value from './Value';
 // import * as actions from '../../redux/counter/counter-actions';
